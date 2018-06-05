cc85a7c484648b9e4bb3e1048c60986469e5246d
Han Ban <hannah15198@gmail.com>
2018-06-05 15:26:37 -0700

    added modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,15 +7,34 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function(request, response) {
-        var difference;
-        var matchName = "";
-        var matchPhoto = "";
-        var matchedScoresArray= [];
+        
+        var newFriendToBeMatched = request.body;
+
+        var newFriendScores =  request.body["scores"];
+
+        var lowestDiff = 90000;
+        var bestMatch;
 
         // absolute value and calculate difference for each user score and friends score
         for(var i = 0; i<friends.length; i++){
-            matchedScoresArray.push(Math.abs(parseInt(req.body.scores[i] - parseInt(friends.scores[i]))))
+            var currentFriend = friends[i];
+            var currentFriendScores = friends[i]["scores"];
+            var totalDif;
+
+           // console.log("currentFriendScores", currentFriendScores)
+            for(var j = 0; j< currentFriendScores.length;j++){
+                totalDif += Math.abs(parseInt(currentFriendScores[j]) - parseInt(newFriendScores[j]));
+            }
+            if(totalDif<lowestDiff){
+                lowestDiff = totalDiff;
+                bestMatch = currentFriend;
+            }
+           // matchedScoresArray.push(Math.abs(parseInt(req.body.scores[i] - parseInt(friends.scores[i]))))
         }
+
+        friends.push(newFriendToBeMatched);
+        // console.log(bestMatch);
+        response.json(bestMatch);
         
     });
 }
